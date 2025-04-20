8aa59071bad9c91daa98a91bf9b5346f41175454
Felix Fricke <felix@frickefamily.de>
2025-04-20 21:15:47 +0200

    Add type validation for input types in FilterOperation to ensure all are ManyToOne

--- llm_graph_optimizer/operations/base_operations/filter_operation.py
+++ llm_graph_optimizer/operations/base_operations/filter_operation.py
@@ -1,7 +1,7 @@
-from typing import Callable
+from typing import Callable, get_origin
 
 from llm_graph_optimizer.graph_of_operations.graph_partitions import GraphPartitions
-from llm_graph_optimizer.graph_of_operations.types import ReasoningState, ReasoningStateType
+from llm_graph_optimizer.graph_of_operations.types import ManyToOne, ReasoningState, ReasoningStateType
 from llm_graph_optimizer.operations.abstract_operation import AbstractOperation
 from llm_graph_optimizer.measurement.measurement import Measurement
 
@@ -12,6 +12,9 @@ class FilterOperation(AbstractOperation):
     """
 
     def __init__(self, output_types: ReasoningStateType, input_types: ReasoningStateType, filter_function: Callable[..., ReasoningState], params: dict = None, name: str = None):
+        # INSERT_YOUR_CODE
+        if not all(get_origin(value) is ManyToOne for value in input_types.values()):
+            raise TypeError("All input types must have keys of origin type ManyToOne")
         self.filter_function = filter_function
         super().__init__(input_types, output_types, params, name)
 
